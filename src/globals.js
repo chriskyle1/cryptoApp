5177cfc650d5874ba4f55b1dfd1fb213897ff72a
chriskyle1 <ckylestem2020@gmail.com>
2022-04-24 12:53:17 -0400

    basic structure for app

---
+++ src/globals.js
@@ -0,0 +1,5 @@
+export const CURRENCY_KEY = 'https://api.exchange.coinbase.com/currencies';
+
+export const CONVERSION_KEY = 'https://api.exchange.coinbase.com/conversions';
+
+export const PRICE_KEY = 'https://api.exchange.coinbase.com/oracle';
